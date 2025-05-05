88573efa990d05f90d4b642b6483853598472154
Alan Souza <rayooszmr@gmail.com>
2025-05-05 16:27:21 -0300

    Update downOrganizer.sh script

--- TaskNLogs/downOrganizer.sh
+++ TaskNLogs/downOrganizer.sh
@@ -6,17 +6,17 @@ echo "[+] Creating directories if not exist."
 DOWNDIR=~/Downloads/
 OLDDIR="$DOWNDIR/Old"
 mkdir -p "$OLDDIR" "$DOWNDIR/Videos" "$DOWNDIR/Documents" "$DOWNDIR/Compressed" "$DOWNDIR/Images" "$DOWNDIR/Scripts"
-CLEANLOG="$DOWNDIR/Downorganizer.log"
+CLEANLOG="$DOWNDIR/downOrganizer.log"
 
-
-echo "[+] Checking for 7 days old files"
+echo -e "\n========== $(date) ==========" | tee -a "$CLEANLOG"
+echo "[+] Checking for 7 days old files" | tee -a "$CLEANLOG"
 
 find "$DOWNDIR" -maxdepth 1 -type f -mtime +7 -print0 | while IFS= read -r -d '' file; do
 	mv "$file" "$OLDDIR"
-	echo "$(date): Moved $file to $OLDDIR" >> "$CLEANLOG"
+	echo "$(date): Moved $file to $OLDDIR" >> "$CLEANLOG" | tee -a "$CLEANLOG"
 done
 
-echo "[+] Analizing extesions and moving"
+echo "[+] Analizing extesions and moving" | tee -a "$CLEANLOG"
 
 find "$DOWNDIR" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
 	# File extesions definition and organizing
@@ -34,13 +34,13 @@ find "$DOWNDIR" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
 
 	if [[ -n "$dest" ]]; then
 		mv "$file" "$dest/"
-		echo "$(date): Moved $file to $dest/" >> "$CLEANLOG"
+		echo "$(date): Moved $file to $dest/" | tee -a "$CLEANLOG"
 	fi
 done
 
-echo "[+] Checking for uncategorized files..."
+echo "[+] Checking for uncategorized files..." | tee -a "$CLEANLOG"
 
-EXCLUDED_NAMES=("Old" "Videos" "Documents" "Compressed" "Images" "Scripts" "cleanup.log")
+EXCLUDED_NAMES=("Old" "Videos" "Documents" "Compressed" "Images" "Scripts" "cleanup.log" "downOrganizer.log")
 GREP_PATERN=$(IFS=\| ; echo "${EXCLUDED_NAMES[*]}")
 LEFTOVERS_FILE="/tmp/leftovers.txt"
 
@@ -57,10 +57,9 @@ done < <(find "$DOWNDIR" -mindepth 1 -maxdepth 1 \( -type f -o -type d \))
 
 
 if [[ -s "$LEFTOVERS_FILE" ]]; then
-	echo "[!] The following items in Downloads were not categorized. Please review manually:"
-	cat "$LEFTOVERS_FILE"
-	echo "[!] Uncategorized items:" >> "$CLEANLOG"
-    cat "$LEFTOVERS_FILE" >> "$CLEANLOG"
+	echo "[!] The following items in Downloads were not categorized. Please review manually:" | tee -a "$CLEANLOG"
+	echo "[!] Uncategorized items:" | tee -a "$CLEANLOG" 
+	cat "$LEFTOVERS_FILE" | tee -a "$CLEANLOG"
 else
 	echo "[+] No uncategorized files or directories left in Downloads."
 fi
